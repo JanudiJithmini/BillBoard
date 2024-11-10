a72cfa694633e66a7b42dc16948e5b044ff4dc99
Janudi <akmeemanajanudi@gamil.com>
2024-11-10 11:18:25 +0530

    Adding BillBoard file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,89 @@
 import Image from "next/image";
+import { IoStarSharp } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 font-DM Sans">
+      <div className="bg-green-950 text-white  p-6 rounded-lg w-80 shadow-lg font-normal">
+        <div className="flex items-center mb-4">
+          <Image
+            src="/images/billboard.jpg"
+            alt="Billboard at Kurunegala"
+            width={70}
+            height={200}
+            className="rounded-md"
+          />
+          <div className="ml-4">
+            <h3 className="text-lg font-DM Sans whitespace-nowrap font-normal">
+              Billboard at Kurunegala
+            </h3>
+            <p className="text-xs">Kurunegala</p>
+            <p className="text-xs flex items-center">
+              <span className="mr-1">
+                <IoStarSharp />
+              </span>
+              5.0
+            </p>
+            <p className="text-xs">Rs.2000.00</p>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="flex items-center text-sm mb-4">
+          <span className="flex items-center mr-2">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/images/greenBulb.png"
+              alt="green bulb"
               width={20}
               height={20}
+              className="rounded-md"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <span className="ml-2">Illumination:</span>
+          </span>
+          <span>No</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="flex items-center text-sm mb-4">
+          <span className="flex items-center mr-2">
+            <Image
+              src="/images/carimage2.png"
+              alt="green bulb"
+              width={20}
+              height={20}
+              className="rounded-md"
+            />
+            <span className="ml-2 font-DM Sans">Traffic count :</span>
+          </span>
+          <span className="font-DM Sans">3246 /per day</span>
+        </div>
+
+        <div className="text-2xl  mb-4">
+          Rs.5000.00 <span className="text-base font-DM Sans">/per week</span>
+        </div>
+        <hr className="border-gray-600 my-4" />
+        <div className="text-xs space-y-2">
+          <div className="flex justify-between font-DM Sans">
+            <span>Adspace fee:</span>
+            <span>Rs.5000.00</span>
+          </div>
+          <div className="flex justify-between font-DM Sans">
+            <span>Service fee:</span>
+            <span>Rs.580.00</span>
+          </div>
+          <div className="flex justify-between font-DM Sans">
+            <span>Tax fee:</span>
+            <span>Rs.0.00</span>
+          </div>
+        </div>
+        <div className="flex justify-between items-center font-DM Sans text-xs mt-4">
+          <span>Total:</span>
+          <span>Rs.5580.00</span>
+        </div>
+        <button
+          className="w-full bg-green-500 text-white  text-sm font-
+DM Sans py-2 mt-4 rounded-3xl	"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Confirm & request
+        </button>
+      </div>
     </div>
   );
 }
